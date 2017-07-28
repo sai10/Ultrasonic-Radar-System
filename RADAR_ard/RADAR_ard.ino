43327397dae78c0d9b09e45b060219b55a92a93f
sai10 <priyankitastro10@gmail.com>
2017-07-29 00:45:16 +0530

    adding code

---
+++ RADAR_ard/RADAR_ard.ino
@@ -0,0 +1,56 @@
+#include<Servo.h>
+#include<NewPing.h>
+
+int trigPin=2;
+int echoPin=3;
+
+long duration;
+int distance;
+Servo servo;
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  servo.attach(9);
+}
+
+void loop()
+{
+  for(int i=15;i<=165;i++)
+  {
+    servo.write(i);
+    delay(100);
+    distance = calculateDistance();
+    
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+  }
+  for(int i=165;i>15;i--)
+  {
+    servo.write(i);
+    delay(100);
+    distance = calculateDistance();
+    
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
+  }
+}
+
+int calculateDistance()
+{
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+  
+  duration = pulseIn(echoPin,HIGH);
+  distance = duration*0.034/2;
+  return distance;
+}  
